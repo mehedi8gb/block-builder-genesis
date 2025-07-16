317be53fe4731ea244f2bbb027a0f43568eb2b19
mehidy <mehidy.gb@gmail.com>
2025-07-17 05:38:55 +0600

    add pm2 config for prod

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+// ecosystem.config.js
+
+module.exports = {
+  apps: [
+    {
+      name: 'block-builder-genesis',
+      script: 'npm',
+      args: 'start',
+      cwd: '/var/www/block-builder-genesis', // Replace with your actual path
+      exec_mode: 'fork', // or 'cluster' if you're scaling
+      instances: 1, // Or use "max" for all CPU cores (only in stateless apps)
+      watch: false, // Set true only if you're watching local file changes
+      env: {
+        NODE_ENV: 'production',
+        PORT: 6000,
+        MONGODB_URI: 'mongodb+srv://mm_mehedi:9hchHyiS7.6JS6H@tms-experiment.hvwuyfv.mongodb.net/?retryWrites=true&w=majority'
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      merge_logs: true,
+      time: true, // adds timestamp to logs
+      restart_delay: 5000,
+    },
+  ],
+};
